bf8c4826f876faff8f5c92c2b428f8d1a352a143
ejean <esteban-jean.pro@hotmail.com>
2025-09-21 15:04:18 +0200

    main.ts was just a test for state management and routing

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,3 @@ export { render, renderElement } from './dom/render';
 export * from './events/handler';
 export * from './router/hash';
 export { Store, globalStore, getState, setState } from './state/store';
-
-console.log('Hello, World!');
